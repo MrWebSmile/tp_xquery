40a16a1d55e8372f1e9c975fa22b98e3af0541ec
MrWebSmile <blabla>
2015-10-14 17:50:04 +0200

    first

---
+++ com.team.Xquery_Java/src/com/team/xquery_java/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team.xquery_java;
+
+import java.io.IOException;
+import org.basex.BaseXServer;
+
+
+/**
+ *
+ * @author adrien
+ */
+public class Main {
+
+    
+    public static void main(String[] args) {
+       try
+       {
+           BaseXServer bs = new BaseXServer();
+           bs.run();
+            
+       }
+       catch(IOException ie)
+       {
+           System.out.println(ie.getMessage());
+       }
+    }
+
+}
