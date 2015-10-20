9300a5664587dcad7b22d40b5f6d421288987668
MrWebSmile <blabla>
2015-10-20 19:34:58 +0200

    ajout createXml

--- tp_xquery/src/xquery/main/Main.java
+++ tp_xquery/src/xquery/main/Main.java
@@ -4,11 +4,10 @@
  * and open the template in the editor.
  */
 package xquery.main;
+
 import xquery.config.Config;
 import xquery.exceptions.SystemException;
 import xquery.model.Model;
-
-
 
 /**
  *
@@ -20,26 +19,27 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         Config conf = new Config(Config.CONF_PTH);
-        Model m = new Model(conf.getProp(Config.DATABASE),conf.getProp(Config.DB_PATH));
-        String xml ="kat.xml";
-        String titre = "Lost Girl";
-        String query = "for $title in //title/text() "
-                + "where contains($title,'"+titre+"')"
-                + "return <p>{$title}</p>";
-        try
-        {
-            String db =m.createDatabase();
+        Model m = new Model(conf.getProp(Config.DATABASE), conf.getProp(Config.DB_PATH));
+        String xml = "kat.xml";
+        String query = "for $title in /series/nom/text()\n"
+                + "for $title2 in //title\n"
+                + "where contains($title2,$title)\n"
+                + "return $title";
+        try {
+            String db = m.createDatabase();
+            String path = conf.getProp(Config.DB_PATH);
             m.openDb(db);
-            m.addXMLToDb(conf.getProp(Config.DB_PATH)+"/"+xml);
+            m.addXMLToDb(path + "/" + xml);
+            m.addXMLToDb(path+ "/series.xml");
             System.out.println(m.getDatabases());
-            System.out.println(m.executeQuery(query));
-        }
-        catch(SystemException se)
-        {
+            String titre = m.executeQuery(query);
+            System.out.println(titre);
+            m.createXml(titre, query, path);
+        } catch (SystemException se) {
             System.out.println(se.getMessage());
         }
     }
-    
+
 }
